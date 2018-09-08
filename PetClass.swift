857c887d4ad05fed0aba0bc22ec805aedcc24330
Tiffany Balcarcel <tiffanybalcarcel@wustl.edu>
2018-09-08 15:34:03 -0500

    Add files via upload

---
+++ PetClass.swift
@@ -0,0 +1,119 @@
+//
+//  PetClass.swift
+//  TiffanyBalcarcel-Lab2
+//
+//  Created by Tiffany Balcarcel on 6/26/18.
+//  Copyright © 2018 Tiffany Balcarcel. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import AudioToolbox
+
+class Pet {
+    
+    enum AnimalType {
+        case dog
+        case cat
+        case bird
+        case bunny
+        case fish
+    }
+    var animalType: AnimalType
+    private (set) var animalPlayed: Int
+    private (set) var animalFed: Int
+    
+    //learned to play audio using several stackoverflow and online resources
+    //got sounds from soundbible.com and freesoundeffects.com (not all of them are public domain oops)
+    
+//    var audioPlayer = AVAudioPlayer()
+//    var hSoundPath = Bundle.main.path(forResource: "", ofType: "mp3")
+//    var fSoundPath = Bundle.main.path(forResource: "", ofType: "mp3")
+    var hSound: SystemSoundID!
+    var fSound: SystemSoundID!
+    
+    
+    init(animalType: AnimalType) {
+        self.animalType = animalType
+        self.animalPlayed = 0
+        self.animalFed = 0
+    }
+    
+    func play() {
+        animalPlayed += 1
+        if animalFed > 0 {
+            animalFed -= 1 //added
+        }
+        else {
+            animalFed = 0
+        }
+    }
+    
+    func makeHSound(resourceName: String) -> SystemSoundID {
+        var hSoundID: SystemSoundID = 0
+        let hSoundURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "\(resourceName)" as CFString?, "mp3" as CFString?, nil)
+        AudioServicesCreateSystemSoundID(hSoundURL!, &hSoundID)
+        return hSoundID
+    }
+    func makeFSound(resourceName: String) -> SystemSoundID {
+        var fSoundID: SystemSoundID = 0
+        let fSoundURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), "\(resourceName)" as CFString?, "mp3" as CFString?, nil)
+        AudioServicesCreateSystemSoundID(fSoundURL!, &fSoundID)
+        return fSoundID
+    }
+    
+    func happySound() {
+//        do { //not sure if this should go here
+////            audioPlayer = try AVAudioPlayer(contentsOf: <#T##URL#>(fileURLWithPath: hSoundPath!))
+//            audioPlayer = try AVAudioPlayer(data: hSound, fileTypeHint: "mp3")
+//            audioPlayer.play()
+//            print("Sound played")
+//        } catch {
+//            print("Could not load audio file")
+//        }
+        switch animalType {
+            case .dog:
+                hSound = makeHSound(resourceName: "bark3")
+            case .cat:
+                hSound = makeHSound(resourceName: "KittenMeow")
+            case .bird:
+                hSound = makeHSound(resourceName: "bluejaycall")
+            case .bunny:
+                hSound = makeHSound(resourceName: "CartoonHop")
+            case .fish:
+                hSound = makeHSound(resourceName: "Bubbles")
+//        default:
+//            print("switch did not work")
+                }
+        AudioServicesPlaySystemSound(hSound)
+    }
+    
+    func feed() {
+        animalFed += 1
+        if animalPlayed > 0 {
+            animalPlayed -= 1 //added
+        }
+        else {
+            animalPlayed = 0
+        }
+    }
+    
+    func yumSound() {
+        switch animalType {
+        case .dog:
+            fSound = makeFSound(resourceName: "bark")
+        case .cat:
+            fSound = makeFSound(resourceName: "CatMeow2")
+        case .bird:
+            fSound = makeFSound(resourceName: "PeacockBird")
+        case .bunny:
+            fSound = makeFSound(resourceName: "prdog3")
+        case .fish:
+            fSound = makeFSound(resourceName: "BubblingEating")
+//        default:
+//            print("switch did not work")
+        }
+        AudioServicesPlaySystemSound(fSound)
+    }
+    
+}
---
+++ ViewController.swift
@@ -0,0 +1,153 @@
+//
+//  ViewController.swift
+//  TiffanyBalcarcel-Lab2
+//
+//  Created by Tiffany Balcarcel on 6/21/18.
+//  Copyright © 2018 Tiffany Balcarcel. All rights reserved.
+//
+
+import UIKit
+import Foundation //not sure if I still need this
+import AVFoundation
+
+class ViewController: UIViewController {
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var animalPic: UIImageView!
+    
+    var pet:Pet! {
+        didSet {
+            updateAnimal(currAnFed: 0, currAnPlayed: 0)
+        }
+    }
+    
+    @IBOutlet weak var happinessView: DisplayView!
+    @IBOutlet weak var playCount: UILabel!
+    @IBOutlet weak var foodView: DisplayView!
+    @IBOutlet weak var foodCount: UILabel!
+    
+    var dog = Pet(animalType: .dog)
+    var cat = Pet(animalType: .cat)
+    var bird = Pet(animalType: .bird)
+    var bunny = Pet(animalType: .bunny)
+    var fish = Pet(animalType: .fish)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pet = dog
+        updateAnimal(currAnFed: dog.animalFed, currAnPlayed: dog.animalPlayed)
+    }
+    
+    @IBAction func dogButton(_ sender: UIButton) {
+        backView.backgroundColor = UIColor.red
+        animalPic.image = #imageLiteral(resourceName: "dog")
+        pet = dog
+        updateAnimal(currAnFed: dog.animalFed, currAnPlayed: dog.animalPlayed)
+    }
+    @IBAction func catButton(_ sender: UIButton) {
+        backView.backgroundColor = UIColor.blue
+        animalPic.image = #imageLiteral(resourceName: "cat")
+        pet = cat
+        updateAnimal(currAnFed: cat.animalFed, currAnPlayed: cat.animalPlayed)
+    }
+
+    @IBAction func birdButton(_ sender: UIButton) {
+        backView.backgroundColor = UIColor.yellow
+        animalPic.image = #imageLiteral(resourceName: "bird")
+        pet = bird
+        updateAnimal(currAnFed: bird.animalFed, currAnPlayed: bird.animalPlayed)
+    }
+    
+    @IBAction func bunnyButton(_ sender: UIButton) {
+        backView.backgroundColor = UIColor.green
+        animalPic.image = #imageLiteral(resourceName: "bunny")
+        pet = bunny
+        updateAnimal(currAnFed: bunny.animalFed, currAnPlayed: bunny.animalPlayed)
+    }
+    
+    
+    @IBAction func fishButton(_ sender: UIButton) {
+        backView.backgroundColor = UIColor.purple
+        animalPic.image = #imageLiteral(resourceName: "fish")
+        pet = fish
+        updateAnimal(currAnFed: fish.animalFed, currAnPlayed: fish.animalPlayed)
+    }
+    
+    func updateAnimal(currAnFed: Int, currAnPlayed: Int) {
+//        happinessView.value = CGFloat(currAnPlayed % 10) / 10
+        happinessView.value = CGFloat(currAnPlayed) / 10
+//        foodView.value = CGFloat(currAnFed % 10) / 10
+        foodView.value = CGFloat(currAnFed) / 10
+        happinessView.animateValue(to: happinessView.value)
+        foodView.animateValue(to: foodView.value)
+        playCount.text = "played: \(currAnPlayed)"
+        foodCount.text = "fed: \(currAnFed)"
+    }
+    
+    @IBAction func playButton(_ sender: UIButton) {
+//        let currentDifference1 = pet.animalPlayed - pet.animalFed
+        if foodView.value <= 0 {
+            return
+        }
+        else {
+            pet.play()
+            pet.happySound()
+            updatePlayButton(currAnPlay: pet.animalPlayed)
+            updateFeedButton(currAnFeed: pet.animalFed)
+        }
+
+    }
+    
+    func updatePlayButton(currAnPlay: Int) {
+        if currAnPlay == 0 {
+            happinessView.value = 0
+        }
+        if currAnPlay > 9 {
+            happinessView.value = 1.0
+        }
+        else {
+//           happinessView.value = CGFloat(currAnPlay % 10) / 10
+            happinessView.value = CGFloat(currAnPlay) / 10
+        }
+        happinessView.animateValue(to: happinessView.value)
+        playCount.text = "played: \(currAnPlay)"
+//        foodView.value -= 0.1
+    }
+    
+    @IBAction func feedButton(_ sender: UIButton) {
+        let currentDifference2 = pet.animalFed - pet.animalPlayed
+        if currentDifference2 >= 10  {
+            return
+        }
+        else {
+            pet.feed()
+            pet.yumSound()
+            updateFeedButton(currAnFeed: pet.animalFed)
+            updatePlayButton(currAnPlay: pet.animalPlayed)
+        }
+    }
+    
+    func updateFeedButton(currAnFeed: Int) {
+        if currAnFeed == 0 {
+            foodView.value = 0
+        }
+        if currAnFeed > 9 {
+            foodView.value = 1.0
+        }
+        else {
+//           foodView.value = CGFloat(currAnFeed % 10) / 10
+            foodView.value = CGFloat(currAnFeed) / 10
+        }
+        foodView.animateValue(to: foodView.value)
+        foodCount.text = "fed: \(currAnFeed)"
+//        happinessView.value -= 0.1
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
